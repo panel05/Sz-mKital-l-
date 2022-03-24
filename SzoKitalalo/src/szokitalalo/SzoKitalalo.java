d72d3b5b2ca04b019a8011cd45d91c59ce33a337
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-03-24 13:01:33 +0100

    kitalal v2

--- SzoKitalalo/src/szokitalalo/SzoKitalalo.java
+++ SzoKitalalo/src/szokitalalo/SzoKitalalo.java
@@ -6,30 +6,63 @@ import java.util.Random;
 public class SzoKitalalo 
 {
     static String [] szoTomb = {"ól", "őz", "tó"};
+    static int N=szoTomb.length;
 
     
     public static void main(String[] args) 
     {
-        kitalal();
-        SzoValasztas();
+        kitalal(SzoValasztas());
+        
+    }
+    
+    static String SzoValasztas() 
+    {
+        Random rnd = new Random();
+        String valasztott = "";
+        int szo = rnd.nextInt(N);
+        valasztott = szoTomb[szo];
+        System.out.println("A gép választott szava: "+valasztott);
+        
+        return valasztott;
     }
 
-    private static void kitalal() 
+    private static void kitalal(String SzoValasztas) 
     {
         Scanner scSzo= new Scanner(System.in);
+        System.out.print("Kérek egy x betűs szót: ");
+        String szoTipp=scSzo.nextLine();
         
+//        int i=0;
+//        while(i<N && !(SzoValasztas==szoTipp))
+//        {
+//            i++;
+//            
+//        }
+        
+        //for (int i = 0; i < SzoValasztas.length(); i++) 
         int i=0;
+        while(i<N && (SzoValasztas!=szoTipp))
+            {
+                i++;
+                if(i<N)
+                {
+                    System.out.printf("van ilyen benne és jó helyen: %d",szoTomb[i].length());
+                }
+                else if(i<N)
+                {
+                    System.out.printf("van ilyen benne, de rossz helyen: %d",szoTomb[i].length());
+                }
+                else
+                {
+                    System.out.print("nincs ilyen benne!");
+                }
+            }
+        
+        
     }
 
 
     
 
-    static void SzoValasztas() 
-    {
-        String valasztott = " ";
-        Random rnd = new Random();
-        int szo = rnd.nextInt(szoTomb.length);
-        valasztott = szoTomb[szo];
-        System.out.println(valasztott);
-    }
+    
 }
