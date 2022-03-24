03c980f0f8f979975a9f56b4eff5080acf61f3a8
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-03-24 15:54:09 +0100

    kitalal v4

--- SzoKitalalo/src/szokitalalo/SzoKitalalo.java
+++ SzoKitalalo/src/szokitalalo/SzoKitalalo.java
@@ -5,12 +5,18 @@ import java.util.Random;
 
 public class SzoKitalalo 
 {
-    static String [] szoTomb = {"öl", "oz", "to"};
+    static String [] szoTomb = {"ol", "oz", "to"};
     static int N=szoTomb.length;
     
     public static void main(String[] args) 
     {
+//        while(!kitalal(SzoValasztas())) {
+//        kitalal(SzoValasztas());
+//            
+//        }
+        
         kitalal(SzoValasztas());
+        System.out.println("sajt");
     }
     
     static String SzoValasztas() 
@@ -24,47 +30,62 @@ public class SzoKitalalo
         return valasztott;
     }
 
-    private static void kitalal(String SzoValasztas) 
+    private static boolean kitalal(String SzoValasztas) 
     {
         Scanner scSzo= new Scanner(System.in);
-        System.out.print("Kérek egy x betűs szót: ");
+        System.out.print("Kérek egy 2 betűs szót: ");
         String szoTipp=scSzo.nextLine();
         String kiir="";
+        boolean helyes=false;
         
         char tippBetu1=szoTipp.charAt(0);
         char tippBetu2=szoTipp.charAt(1);
         char valasztottBetu1=SzoValasztas.charAt(0);
         char valasztottBetu2=SzoValasztas.charAt(1);
         
+        while(!(szoTipp.equals(SzoValasztas)))
+        {
+            
+            if(tippBetu1==valasztottBetu1)
+            {
+                kiir="jó az első betű.";
+            }
+            else //if(tippBetu1!=valasztottBetu1)
+            {
+                kiir="nem jó az első betű.";
+                kitalal(SzoValasztas);
+            }
+            System.out.println(kiir);
+
+            if(tippBetu2==valasztottBetu2)
+            {
+                kiir="jó a második betű.";
+            }
+            else //if(tippBetu2!=valasztottBetu2)
+            {
+                kiir="nem jó a második betű.";
+                kitalal(SzoValasztas);
+            }
+            System.out.println(kiir);
+
+            if(tippBetu1==valasztottBetu1 && tippBetu2==valasztottBetu2)
+            {
+                kiir="Eltaláltad a szót!";
+                helyes = true;
+            }
+            else
+            {
+                kiir="Nem találtad el!";
+                kitalal(SzoValasztas);
+            }
+            
+            System.out.println(kiir);
+            
+            
+        }
         
-        if(tippBetu1==valasztottBetu1)
-        {
-            kiir="jó az első betű.";
-        }
-        else //if(tippBetu1!=valasztottBetu1)
-        {
-            kiir="nem jó az első betű.";
-        }
-        System.out.println(kiir);
-        
-        if(tippBetu2==valasztottBetu2)
-        {
-            kiir="jó a második betű.";
-        }
-        else //if(tippBetu2!=valasztottBetu2)
-        {
-            kiir="nem jó a második betű.";
-        }
-        System.out.println(kiir);
-        
-        if(tippBetu1==valasztottBetu1 && tippBetu2==valasztottBetu2)
-        {
-            kiir="Eltaláltad a szót!";
-        }
-        else
-        {
-            kiir="Nem találtad el!";
-        }
-        System.out.println(kiir);
+        return helyes;
     }
+    
+    
 }
