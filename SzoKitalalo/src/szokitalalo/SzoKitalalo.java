94bcc80239ced72cd22436bcf174b8c877ce8fb0
panel05 <pantlbalazs@gmail.com>
2022-03-24 12:04:47 +0100

    java file

---
+++ SzoKitalalo/src/szokitalalo/SzoKitalalo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szokitalalo;
+
+/**
+ *
+ * @author szf1_b
+ */
+public class SzoKitalalo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
