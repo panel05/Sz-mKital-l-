f68835f606d27fd84e5c9817d511113d757268ca
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-03-24 12:24:54 +0100

    kitalal

--- SzoKitalalo/src/szokitalalo/SzoKitalalo.java
+++ SzoKitalalo/src/szokitalalo/SzoKitalalo.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package szokitalalo;
 
-/**
- *
- * @author szf1_b
- */
-public class SzoKitalalo {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class SzoKitalalo {
+    
+    public static void main(String[] args) 
+    {
+        kitalal();
+    }
+
+    private static void kitalal() 
+    {
+        Scanner scSzo= new Scanner(System.in);
+        
+        int i=0;
+        
     }
     
 }
